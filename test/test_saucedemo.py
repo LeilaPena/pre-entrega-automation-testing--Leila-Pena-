5cbdfad12f070d0adbf806ead92f020e10e908eb
Leila <leilajulieta@gmail.com>
2025-10-23 23:35:28 -0300

    Agregue funcion para testear el carrito

--- test/test_saucedemo.py
+++ test/test_saucedemo.py
@@ -2,6 +2,7 @@ import pytest
 import sys
 import os
 from selenium.webdriver.common.by import By
+import time
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
 
 from utils.helpers import login_saucedemo, get_driver
@@ -27,7 +28,14 @@ def test_catalogo(driver):
     assert len(products) > 0 #verifica que existan productos
 
 def test_carrito(driver):
-    login_saucedemo()
+    login_saucedemo(driver)
     products = driver.find_elements(By.CLASS_NAME, 'inventory_item')
     total_products = len(products)
-    products[0].find_elements(By.TAG_NAME, 'button').click()
+
+    if total_products >= 2:
+        products[0].find_element(By.TAG_NAME,'button').click()
+        products[1].find_element(By.TAG_NAME,'button').click()
+
+        time.sleep(5)
+        badge = driver.find_element(By.CLASS_NAME,'shopping_cart_badge').text
+        assert badge == '2'
