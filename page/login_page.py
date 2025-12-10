6a3be40c462ce645347ae44b98a437e0e9c13f61
Leila Peña <leilajulieta1999@gmail.com>
2025-12-10 17:32:34 -0300

    Agregue contenido hasta la clase 13

---
+++ page/login_page.py
@@ -0,0 +1,22 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from utils.helpers import URL
+from selenium.webdriver.common.by import By
+
+class LoginPage:
+
+    campo_username = (By.NAME, 'user-name')
+    campo_password = (By.NAME, 'password')
+    login_button = (By.NAME, 'login-button')
+    def __init__(self,  driver):
+        self.driver = driver
+
+    def open (self):
+        self.driver.get(URL)
+
+    def login(self, username, password):
+
+        WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable(self.campo_password)).send_keys(password) #valida si existe el campo password
+        WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable(self.campo_username)).send_keys(username) #valida si existe el campo username
+        WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable(self.login_button)).click() #valida si existe el login button
+
--- utils/helpers.py
+++ utils/helpers.py
@@ -3,25 +3,23 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
 
 URL = 'https://www.saucedemo.com/'
-username = "standard_user"
-password = "secret_sauce"
 
 def get_driver(): #función para que los servicios se actualicen solos
     options = Options()
     options.add_argument('--start-maximized') #Comienza con la pestaña en pantalla completa
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
+    service = Service(ChromeDriverManager().install()) #instalar el driver
+    driver = webdriver.Chrome(service=service, options=options) #instalar el driver
     driver.implicitly_wait(10)
 
     return driver
 
-def login_saucedemo(driver):
-    driver.get(URL)
+def get_file_path(file_name, folder="data"):
+    current_file = os.path.dirname(__file__) #archivo actual
+    file_path = os.path.join(current_file,"..",folder,file_name) #Ruta relativa
 
-    #ingresar las credenciales
-    driver.find_element(By.NAME,"user-name").send_keys(username)
-    driver.find_element(By.NAME,"password").send_keys(password)
-    driver.find_element(By.ID,'login-button').click()
-    
+    return os.path.abspath(file_path) #Ruta absoluta
