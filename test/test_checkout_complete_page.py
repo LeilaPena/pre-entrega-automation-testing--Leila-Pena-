f3f5911e28e065a6f4d09f1bc63e67dd22a2a956
Leila Peña <leilajulieta1999@gmail.com>
2025-12-10 20:45:35 -0300

    Agregue checkout complete

---
+++ test/test_checkout_complete_page.py
@@ -0,0 +1,35 @@
+from page.login_page import LoginPage
+from page.inventory_page import InventoryPage
+from page.cart_page import CartPage
+from page.checkout_page import CheckoutPage
+from page.checkout_complete_page import CheckoutCompletePage
+import time
+
+def test_complete_purchase_flow(driver):
+    login = LoginPage(driver)
+    inventory = InventoryPage(driver)
+    cart = CartPage(driver)
+    checkout = CheckoutPage(driver)
+    complete = CheckoutCompletePage(driver)
+
+    login.open()
+    login.login("standard_user", "secret_sauce")
+
+    time.sleep(4)
+    
+    inventory.add_product_to_cart(0)
+    inventory.go_to_cart()
+    
+    cart.go_to_checkout()
+    
+    checkout.fill_customer_info("John", "Doe", "12345")
+    checkout.continue_to_overview()
+    
+    driver.get("https://www.saucedemo.com/checkout-complete.html")
+    
+    assert complete.is_at_page()
+    assert "Thank you for your order!" in complete.get_success_message()
+    assert complete.is_success_image_displayed()
+    
+    complete.back_to_home()
+    assert inventory.is_at_page()
